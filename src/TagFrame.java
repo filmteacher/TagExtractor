1ec74603499d3137a52114f1342d3a39e8dd0008
Matt Bennett <bennetrd@ucmail.uc.edu>
2025-11-01 19:44:45 -0400

    Interface designed

--- src/TagFrame.java
+++ src/TagFrame.java
@@ -1,4 +1,189 @@
-package PACKAGE_NAME;
+import javax.swing.*;
+        import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
+        import java.awt.event.ActionEvent;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+        import java.util.List;
+import java.util.stream.Stream;
 
-public class TagFrame {
+/*
+/ Comp Programming II - Lab08 - Tag Extractor
+/ @author Matt Bennett
+*/
+
+public class TagFrame extends JFrame
+{
+    JPanel mainPnl;
+    JPanel titlePnl;
+    JPanel filesPnl;
+    JPanel textPnl;
+    JPanel buttonsPnl;
+
+    JLabel title;
+
+    JButton fileBtn;
+    JLabel fileLbl;
+    JTextField fileFld;
+
+    JButton stopBtn;
+    JLabel stopLbl;
+    JTextField stopFld;
+
+    JTextArea resultsArea;
+    JScrollPane resultsScroller;
+
+    JButton saveBtn;
+    JButton quitBtn;
+
+    int marginSize = 15;
+
+    public TagFrame()
+    {
+        createMainPnl();
+
+        createTitlePnl();
+        mainPnl.add(titlePnl);
+
+        createFilesPnl();
+        mainPnl.add(filesPnl);
+
+        createTextPnl();
+        mainPnl.add(textPnl);
+
+        createButtonsPnl();
+        mainPnl.add(buttonsPnl);
+
+        add(mainPnl);
+
+        setTitle("Comp Prog II - Lab08 - Tag Extractor");
+        setSize(400, 765);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    private void createMainPnl() {
+        mainPnl = new JPanel();
+        mainPnl.setLayout(new FlowLayout(FlowLayout.CENTER, 0, marginSize));
+        mainPnl.setBackground(Color.LIGHT_GRAY);
+    }
+
+    private void createTitlePnl() {
+        titlePnl = new JPanel();
+        title = new JLabel("<html>Extract Tags/Keywords<br />from a Text File</html>");
+        title.setFont(new Font("Verdana", Font.BOLD, 22));
+        titlePnl.add(title);
+        titlePnl.setBackground(Color.LIGHT_GRAY);
+    }
+
+    private void createFilesPnl() {
+        filesPnl = new JPanel();
+        filesPnl.setLayout(new GridLayout(2, 2));
+        filesPnl.setBackground(Color.WHITE);
+        filesPnl.setPreferredSize(new Dimension(360, 80));
+
+        stopFld = new JTextField();
+
+        stopBtn = new JButton("Choose Stop Words File");
+
+        fileFld = new JTextField();
+
+        fileBtn = new JButton("Choose Literature File");
+//        fileBtn.addActionListener((ActionEvent ae) ->
+//        {
+//            if (selectedFile == null) {
+//                JOptionPane.showMessageDialog(this, "Please load a file first!",
+//                        "No File Selected", JOptionPane.WARNING_MESSAGE);
+//                return;
+//            }
+//
+//            String searchString = searchFld.getText();
+//
+//            if (Objects.equals(searchString, "")) {
+//                JOptionPane.showMessageDialog(this, "Enter a search string first!",
+//                        "No Search String Entered", JOptionPane.WARNING_MESSAGE);
+//                return;
+//            }
+//
+//            Path file = selectedFile.toPath();
+//
+//            resultsArea.setText("");
+//
+//            try (Stream<String> lines = Files.lines(file))
+//            {
+//                lines
+//                        .filter(l -> l.contains(searchString))
+//                        .forEach(l -> resultsArea.append(l + "\n\n"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+
+        filesPnl.add(stopBtn);
+        filesPnl.add(stopFld);
+        filesPnl.add(fileBtn);
+        filesPnl.add(fileFld);
+    }
+
+    private void createTextPnl() {
+        textPnl = new JPanel();
+        textPnl.setLayout(new GridLayout(1, 2));
+        textPnl.setBackground(Color.WHITE);
+
+        resultsArea = new JTextArea(25, 25);
+        resultsArea.setFont(new Font("Verdana", Font.PLAIN, 14));
+        resultsArea.setEditable(false);
+        resultsArea.setEditable(false);
+        resultsArea.setLineWrap(true);
+        resultsArea.setWrapStyleWord(true);
+        resultsScroller = new JScrollPane(resultsArea);
+        resultsScroller.setBorder(new TitledBorder(new EtchedBorder(), "Tags/Keywords::"));
+        textPnl.add(resultsScroller);
+    }
+
+    private void createButtonsPnl() {
+        buttonsPnl = new JPanel();
+        buttonsPnl.setLayout(new GridLayout(1, 2));
+        buttonsPnl.setBackground(Color.WHITE);
+        buttonsPnl.setPreferredSize(new Dimension(360, 40));
+
+        saveBtn = new JButton("SAVE results");
+//    saveBtn.addActionListener((ActionEvent ae) ->
+//    {
+//        JFileChooser chooser = new JFileChooser();
+//
+//        File workingDirectory = new File(System.getProperty("user.dir"));
+//
+//        chooser.setCurrentDirectory(workingDirectory);
+//
+//        if (chooser.showOpenDialog(null) == JFileChooser.APPROVE_OPTION) {
+//            this.selectedFile = chooser.getSelectedFile();
+//            Path file = selectedFile.toPath();
+//
+//            originalArea.setText("");
+//
+//            try (Stream<String> lines = Files.lines(file))
+//            {
+//                lines
+//                        .forEach(l -> originalArea.append(l + "\n"));
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    });
+
+        quitBtn = new JButton("QUIT the app");
+        quitBtn.addActionListener((ActionEvent ae) ->
+        {
+            System.exit(0);
+        });
+
+        buttonsPnl.add(saveBtn);
+        buttonsPnl.add(quitBtn);
+    }
 }
--- src/TagRunner.java
+++ src/TagRunner.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
+import javax.swing.*;
 
-public class TagRunner {
+public class TagRunner
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new TagFrame();
+    }
 }
